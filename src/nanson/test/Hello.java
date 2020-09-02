7272b1a55135eb4759cc6eb9f1d887b07dc2f134
hejincheng <13648342687@163.com>
2020-09-02 14:31:53 +0800

    init

--- src/nanson/test/Hello.java
+++ src/nanson/test/Hello.java
@@ -4,4 +4,9 @@ package nanson.test;
  * @Author hejincheng
  * @Date 2020/9/2
  */
-public class Hello {}
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
